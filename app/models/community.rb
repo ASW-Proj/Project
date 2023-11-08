805bf5ef3855d89487e93ef79c814cd28111782d
xavierviguera <xavier.viguera.dev@gmail.com>
2023-11-08 16:36:07 +0100

    Linked posts with communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,5 +2,5 @@ class Community < ApplicationRecord
     has_one_attached :avatar
     has_one_attached :banner
     validates :c_id, presence: true, uniqueness: true
-    has_many :posts
+    has_many :posts, dependent: :restrict_with_exception
 end
