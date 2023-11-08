87136ff5f9cd4daa03bb2e4cecd8e54cc98f717a
xavierviguera <xavier.viguera.dev@gmail.com>
2023-11-08 15:30:25 +0100

    fixed images

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,5 +2,6 @@ class Community < ApplicationRecord
     has_one_attached :avatar
     has_one_attached :banner
     validates :c_id, presence: true
+    validates_uniqueness_of :c_id
     has_many :posts
 end
