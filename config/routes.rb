5bb7f98d71ec143a4eb39321823a974bef24eb20
Gisela Borràs <gisela.borras.zaplana@estudiantat.upc.edu>
2023-11-14 00:06:00 +0000

    Visualitzacio Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
@@ -10,46 +9,27 @@ Rails.application.routes.draw do
     get '/logout', to: 'users/sessions#destroy', as: :logout
   end
 
-
-
-
   resources :communities
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-
-  # ...................................................................... #
-  # To create a post, we call the create function from the posts controller.
-  #get "/create_post", to: "posts#create", as: :post
-  resources :posts
+  # Resources for posts and comments
   resources :posts do
     resources :comments, only: [:create]
   end
 
+  # Search route
   get 'search', to: 'search#index'
+  get 'search/index'
 
-
+  # User routes
   resources :users, only: [:show] do
     member do
       get 'posts', to: 'users#show', content_type: 'Publicaciones', as: 'user_posts'
       get 'comments', to: 'users#comments', as: 'user_comments'
     end
   end
+  get '/home_newt_posts', to: 'posts#home_newt', as: :home_newt_posts
 
-  get 'search/index'
-
-  resources :users
-
-  # Route to view a single post
-  get "/posts/:id" => "posts#show", as: :show_post
-
-  get '/buscar', to: 'posts#buscar'
-
+  # Root route
   root 'posts#home'
 end
