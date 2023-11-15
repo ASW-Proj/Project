cdae4f93151039d75223cd8cc582debc8c51688c
xavierviguera <xavier.viguera.dev@gmail.com>
2023-11-15 21:03:00 +0100

    Subscribers!

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_one_attached :banner
   has_many :posts
   has_many :comments
-  has_and_belongs_to_many :communities
+  has_many :subscriptions, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
