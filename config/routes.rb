4b533e2fadc15abd82c7e1460275501bd476c5dc
Ubuntu <ignacio.folgueiras@estudiantat.upc.edu>
2023-11-15 22:42:26 +0000

    Fix ruta de arriba, fix botones homepage

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # resources :subscriptions, only: :create
   get "/subscribe/:community_id", to: "subscriptions#create", as: "subscribe"
   get "/unsubscribe/:community_id", to: "subscriptions#destroy", as: "unsubscribe"
-  
+
   # Resources for communities
   resources :communities
 
@@ -29,14 +29,14 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
-    
+
     member do
       post 'vote_up'
       post 'vote_down'
     end
   end
   resources :posts do
-    resources :comments 
+    resources :comments
   end
   resources :posts do
     resources :comments, only: [:create, :new, :destroy] do
@@ -66,6 +66,7 @@ resources :users
   post '/save_post/:post_id', to: 'saved_items#save_post', as: 'save_post'
   post '/save_comment/:comment_id', to: 'saved_items#save_comment', as: 'save_comment'
   get '/list_saved_items', to: 'saved_items#list_saved_items', as: 'list_saved_items'
+  get '/home', to: 'posts#home', as: 'frontpage'
   # Root route
   root 'posts#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_15_191347) do
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,7 +73,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_191347) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-
   create_table "saved_comments", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "comment_id", null: false
@@ -88,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_191347) do
     t.index ["post_id"], name: "index_saved_posts_on_post_id"
     t.index ["user_id"], name: "index_saved_posts_on_user_id"
   end
+
   create_table "subscriptions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "community_id", null: false
