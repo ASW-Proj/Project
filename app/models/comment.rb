2e7cc3b05ba33f2a2fc88335ef428caf983845cf
Ubuntu <ignacio.folgueiras@estudiantat.upc.edu>
2023-11-16 12:01:53 +0000

    Filtros y order avanzados

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, counter_cache: true
   belongs_to :community
   has_many :likes, dependent: :destroy
 
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :parent, class_name: 'Comment', optional: true
-  
+
   has_many :saved_comments, dependent: :destroy
   has_many :savers, through: :saved_comments, source: :user
 
